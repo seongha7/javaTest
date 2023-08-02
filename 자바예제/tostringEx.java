5804d76ee5f6177e8b9e0ef46f76cb47cbffc8d6
seongha7 <140695961+seongha7@users.noreply.github.com>
2023-08-02 15:37:23 +0900

    Create tostringEx.java

---
+++ 자바예제/tostringEx.java
@@ -0,0 +1,14 @@
+package com.example.javatest;
+import java.util.*;
+
+class equalsEx {
+    public static void main(String[] args) {
+        String str = new String("korea");
+        Date today = new Date();
+
+        System.out.println(str);
+        System.out.println(str.toString());
+        System.out.println(today);
+        System.out.println(today.toString());
+    }
+}
