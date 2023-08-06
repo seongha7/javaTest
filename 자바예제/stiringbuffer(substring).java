e4b33b0254649db768e4c7665bd12b26bad8a718
seongha7 <140695961+seongha7@users.noreply.github.com>
2023-08-06 16:40:20 +0900

    Create stiringbuffer(substring).java

---
+++ 자바예제/stiringbuffer(substring).java
@@ -0,0 +1,10 @@
+package com.example.javatest;
+
+
+class exceptionEx {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("Hello jump to java");
+        System.out.println(sb.substring(11, 13));
+    }
+}
