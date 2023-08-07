a505e1468c5baa9d9eec413ef028ac3f662718e3
seongha7 <140695961+seongha7@users.noreply.github.com>
2023-08-07 18:07:35 +0900

    Create 20230807 generic.java

---
+++ 자바예제/20230807 generic.java
@@ -0,0 +1,43 @@
+package com.example.javatest;
+import java.util.ArrayList;
+
+class Fruit {
+    public String toString() {
+        return "Fruit";
+    }
+}
+class Apple extends Fruit{ public String toString() {return "Apple";}}
+class Grape extends Fruit{ public String toString() {return "Grape";}}
+class Toy{ public String toString() {return "Toy";}}
+
+class FruitBoxEx1 {
+    public static void main(String[] args) {
+        Box<Fruit> fruitBox = new Box<>();
+        Box<Apple> appleBox = new Box<>();
+        Box<Toy> toyBox = new Box<>();
+        Box<Grape> grapeBox = new Box<>();
+
+        // 상속관계라 가능
+        fruitBox.add(new Fruit());
+        fruitBox.add(new Apple());
+        fruitBox.add(new Grape());
+
+        // 각각의 것들만 담을 수 있음(apple 에는 apple 만 가능)
+        appleBox.add(new Apple());
+        grapeBox.add(new Grape());
+        toyBox.add(new Toy());
+
+        System.out.println(fruitBox);
+        System.out.println(appleBox);
+        System.out.println(grapeBox);
+        System.out.println(toyBox);
+    }
+}
+
+class Box<T> {
+    ArrayList<T> list = new ArrayList<>();
+    void add(T item) { list.add(item);}
+    T get (int i) { return list.get(i);}
+    int size() { return list.size();}
+    public String toString() { return list.toString();}
+}
