e52f99324bd15c8511d0f3c874e261923149fdc3
seongha7 <140695961+seongha7@users.noreply.github.com>
2023-08-10 11:22:17 +0900

    Create 230810 ThreadGroup.java

---
+++ 자바예제/230810 ThreadGroup.java
@@ -0,0 +1,32 @@
+package com.example.javatest;
+
+
+
+class ThreadEx {
+    public static void main(String[] args) {
+        ThreadGroup main = Thread.currentThread().getThreadGroup();
+        ThreadGroup grp1 = new ThreadGroup("Group1");
+        ThreadGroup grp2 = new ThreadGroup("Group2");
+
+        ThreadGroup subGrp1 = new ThreadGroup(grp1, "SubGroup1");
+
+        grp1.setMaxPriority(3);
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+        new Thread(grp1, r, "th1").start();
+        new Thread(subGrp1, r, "th2").start();
+        new Thread(grp2, r, "th3").start();
+
+        System.out.println(">>List of ThreadGroup : " + main.getName() + ", Active ThreadGroup: " + main.activeGroupCount() + ", Active Thread: " + main.activeCount());
+        main.list(); // main 쓰레드 그룹의 정보 호출
+
+    }
+}
