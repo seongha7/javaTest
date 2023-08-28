959b0e1b48fa50bb6186d6d01af9d05dfaccdcb5
seongha7 <140695961+seongha7@users.noreply.github.com>
2023-08-28 11:43:39 +0900

    Create 매개변수.java

---
+++ 자바기초/매개변수.java
@@ -0,0 +1,13 @@
+package com.example.javatest;
+
+
+public class Sample {
+    static int sum(int a, int b) {         // int a , int b 가 매개변수
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(4,8));  // 4,8은 인자
+    }
+
+}
