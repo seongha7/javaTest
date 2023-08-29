c2023fcf4ff37943a91b1f51f0f2803e7a193eef
seongha7 <140695961+seongha7@users.noreply.github.com>
2023-08-29 17:26:13 +0900

    Update 인터페이스.java

--- 자바기초/인터페이스.java
+++ 자바기초/인터페이스.java
@@ -42,3 +42,41 @@ public class Sample {
         cafe.printCoffee();
     }
 }
+// 상속 예시 
+
+package com.example.javatest;
+
+
+class ResCustomer {
+    public String resCustomer;
+
+    public void setResCustomer(String resCustomer) {
+        this.resCustomer = resCustomer;
+    }
+}
+
+class ResCustomer1 extends ResCustomer{}
+
+class ResCustomer2 extends ResCustomer{}
+
+class Resowner {
+    public void giveFood(ResCustomer1 resCustomer1) {
+        System.out.println("1에게 제육을 준다");
+    }
+
+    public void giveFood(ResCustomer2 rescustomer2) {
+        System.out.println("2에게 돈까스를 준다");
+    }
+
+}
+
+public class Sample {
+    public static void main(String[] args) {
+        Resowner resowner = new Resowner();
+        ResCustomer1 a = new ResCustomer1();
+        ResCustomer2 b = new ResCustomer2();
+
+        resowner.giveFood(a);
+        resowner.giveFood(b);
+    }
+}       // 만약 손님이 늘어난다면, 만들어야 할 오버로딩된 메서드가 늘어나게 된다.
