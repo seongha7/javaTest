2e8201b32b8a47c57c41600d865ce0e3a205ef2f
seongha7 <140695961+seongha7@users.noreply.github.com>
2023-08-29 12:01:46 +0900

    Update 상속.java

--- 자바기초/상속.java
+++ 자바기초/상속.java
@@ -25,3 +25,33 @@ class Sample {
         dog.sleep();
     }
 }
+
+// 예시 2
+
+package com.example.javatest;
+
+
+
+ class ParentCafe {
+    String coffee;
+    int price;
+
+    public void printMenu() {
+        System.out.println(coffee + "는 " + price + "원입니다.");
+    }
+}
+
+class ChildCafe extends ParentCafe {
+    ChildCafe (String coffee, int price) {
+        this.coffee = coffee;
+        this.price = price;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        //ChildCafe child = new ChildCafe("아메리카노", 4100);
+        ParentCafe child = new ChildCafe("아메리카노" , 4000); // 상속관계에 있을때 자식 클래스의 객체는 부모 클래스의 자료형인 것처럼 사용 가능
+        child.printMenu();
+    }
+}
