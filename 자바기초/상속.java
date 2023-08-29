aa3900b0dcad09df3cd54062569e90d90a93f46e
seongha7 <140695961+seongha7@users.noreply.github.com>
2023-08-29 13:25:23 +0900

    Update 상속.java

--- 자바기초/상속.java
+++ 자바기초/상속.java
@@ -10,8 +10,8 @@ class Animal {
     }
 }
 
-// animal 클래스 상속
-class Dog extends Animal {
+
+class Dog extends Animal {        // animal 클래스 상속
     void sleep() {
         System.out.println(this.name + " zzz");
     }
@@ -53,5 +53,43 @@ public class Main {
         //ChildCafe child = new ChildCafe("아메리카노", 4100);
         ParentCafe child = new ChildCafe("아메리카노" , 4000); // 상속관계에 있을때 자식 클래스의 객체는 부모 클래스의 자료형인 것처럼 사용 가능
         child.printMenu();
+    }
+}
+
+// 예제 3
+package com.example.javatest;
+
+
+
+class Animal {
+    String name;
+
+    void setName(String name) {
+        this.name = name;
+    }
+}
+
+class Dog extends Animal {
+    void sleep() {
+        System.out.println(this.name + " 자는중");
+    }
+}
+
+class HouseDog extends Dog {
+    void sleep() {
+        System.out.println(this.name + " 자는중 in house");
+    }
+
+    void sleep(int hour) {      // 입력 항목으로 hour라는 int 자료형 추가
+        System.out.println(this.name + " 자는중 in house 에서 " + hour + " 시간동안");
+    }
+}
+
+public class Sample {
+    public static void main(String[] args) {
+        HouseDog houseDog = new HouseDog();
+        houseDog.setName("puppy");
+        houseDog.sleep();
+        houseDog.sleep(3);
     }
 }
