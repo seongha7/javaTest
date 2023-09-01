399858d45d375b7787e564165fae878c3bd05012
seongha7 <140695961+seongha7@users.noreply.github.com>
2023-09-01 13:31:47 +0900

    Create 성적관리프로그램.java

---
+++ 자바기초/성적관리프로그램.java
@@ -0,0 +1,30 @@
+package com.example.javatest;
+
+/*  -한 메서드에 3단계 들여쓰기만 하기
+    - else 키워드 쓰지 않기
+    - 이름 줄여쓰기 않기
+    - getter(사용 가능) setter(아에 사용 x) 를 쓰지 않는다.
+자바로 성적관리프로그램 구현하기 -> 점수에 따라 등급 분류
+등급이 낮은 학생과 높은 학셍에 따라 클래스 분류 및 점수가 낮은 학생들을 추가교육 실시
+class 학교 학생 과목 점수 학급 -> 너무 간단하게 만들지 말기
+성적 관리 프로그램에 필요한것? 성적을 관리해줄 선생님 필수, 점수에 따른 등급분류? 필요 */
+
+class School {}
+// 학교 안 학급 클래스 구성
+class Standford {}
+class Seoul {}
+class Harvard {}
+
+
+class Student {}
+// 학생 세부 분류 필요
+class Teacher {}
+// 추가교육 클래스
+class ExtraEducation {}
+// 등급분류 클래스
+class Grade {}
+
+//과목
+class English {}
+class Korean {}
+class Mathematics {}
