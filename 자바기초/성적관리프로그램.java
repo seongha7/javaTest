e6e7b99490bb1a26ff40e946ede5d37b76fc9592
seongha7 <140695961+seongha7@users.noreply.github.com>
2023-09-04 11:01:27 +0900

    Update 성적관리프로그램.java

--- 자바기초/성적관리프로그램.java
+++ 자바기초/성적관리프로그램.java
@@ -62,3 +62,69 @@ public class Sample {
 
     }
 }
+
+
+// scanner 활용한 성적관리 프로그램 
+
+import java.util.Scanner;
+
+class Student {
+    // 학생 객체 생성
+     String name;
+     int math;
+     int english;
+     int korean;
+
+    public Student(String name,int math,int english,int korean){
+        this.name = name;
+        this.math = math;
+        this.english = english;
+        this.korean = korean;
+    }
+
+    public double SumOfGrade(){
+        return math+korean+english;
+    }
+    public double Average(){
+
+        return (math+korean+english)/3;
+    }
+
+}
+
+public class Sample {
+    public static void main(String[] args) {
+        int studentNum = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("학생 수를 입력하세요");
+        studentNum = sc.nextInt();
+
+        Student[] student = new Student[studentNum];
+        System.out.println("학생이름 수학점수 영어점수 국어점수 순으로 입력하시오(성하 100 100 100)");
+
+
+        for (int i = 0; i < student.length; i++) {
+            String name = scanner.next();
+            int math = scanner.nextInt();
+            int english = scanner.nextInt();
+            int korean = scanner.nextInt();
+
+            student[i] = new Student(name,math,english,korean);
+        }
+        System.out.println("----출력예시입니다-----");
+        System.out.println("학생이름 + 수학점수 + 영어점수 + 국어점수 + 총점 + 평균");
+        System.out.println("------------------");
+
+        for (int i = 0; i < student.length; i++) {
+            System.out.print("학생이름: " + student[i].name
+                    + " 수학성적: " + student[i].math + " 영어성적: " + student[i].english + " 국어성적: " + student[i].korean);
+            System.out.print(" 총점: " + student[i].SumOfGrade());
+            System.out.println(" 평균: " + student[i].Average());
+        }
+        scanner.close();
+        sc.close();
+    }
+}
